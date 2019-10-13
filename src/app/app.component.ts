85d27da342e5047c9ad52170842f90924428f68d
Manepalli Shravani <30730507+manepallishravani@users.noreply.github.com>
2019-10-13 23:28:59 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+    LoginForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) {
+       this.LoginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        }); }
+
+    
+
+    // convenience getter for easy access to form fields
+    get f() { return this.LoginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.LoginForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!!\n\n' + JSON.stringify(this.LoginForm.value))
+    }
+}
